7239f285c41b4c7c112d51925578440ceacc55ab
nanan@softserveinc.com <nanan@DP263.softservecom.com>
2015-10-29 14:14:31 +0200

    Fix navigation

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ function checkForTFS(tab) {
 	var a = document.createElement ('a');
 
     a.href = tab.url;
-    if (a.hostname == "eservicestfs.nt.local") {
+    if (/tfs/i.test(tab.url.split('/').slice(0,4).join('/'))) {
         chrome.pageAction.show(tab.id);
     }
 };
